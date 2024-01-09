60c096a3303ccd921c25a3adf7620d4729cf524a
tadahiroueta <tadahiroueta@gmail.com>
2024-01-09 09:17:27 -0600

    Initial Commit

---
+++ traffic-segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic-segues
+//
+//  Created by Ueta, Lucas T on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ traffic-seguesUITests/traffic_seguesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  traffic_seguesUITestsLaunchTests.swift
+//  traffic-seguesUITests
+//
+//  Created by Ueta, Lucas T on 1/9/24.
+//
+
+import XCTest
+
+final class traffic_seguesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
